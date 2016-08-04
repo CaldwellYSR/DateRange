ec4eac6c6eac79be56786bde7d01f9f55bb6875d
Matthew Caldwell <caldwellysr@gmail.com>
2016-08-04 17:59:20 -0400

    add IDs to inputs and validate country code

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var DateRange = function(date, dayNum, country, apiKey) {
+var DateRange = function(date, dayNum, country, errors, apiKey) {
     this.date = date;
     this.dayNum = dayNum;
     this.country = country;
+    this.errors = errors;
     this.apiKey = apiKey;
     this.validCountries = [
         "BE", "BG", "BR", "CA", "DE", 
@@ -13,15 +14,30 @@ var DateRange = function(date, dayNum, country, apiKey) {
 };
 
 DateRange.prototype.validateCountryCode = function(e) {
+    e.target.value = e.target.value.toUpperCase();
+    if (e.target.value.length < 2) {
+        return;
+    }
+    if (e.target.value.length > 2) {
+        e.target.value = e.target.value.substr(0, 2);
+        console.log(this.validCountries.indexOf(e.target.value));
+        return;
+    }
+    if (this.validCountries.indexOf(e.target.value) < 0) {
+        this.errors.innerHTML = "Please input a valid country code";
+    } else {
+        this.errors.innerHTML = "";
+    }
 };
 
 function initDateRange() {
     var date = document.getElementById("date");
     var dayNum = document.getElementById("days");
     var country = document.getElementById("country");
+    var errors = document.getElementById("errors");
 
     // TODO find a way to mask api key
-    var dateRange = new DateRange(date, dayNum, country, '71a68222-7099-442c-b17f-10c6dd903d9c');
+    var dateRange = new DateRange(date, dayNum, country, errors, '71a68222-7099-442c-b17f-10c6dd903d9c');
 }
 
 window.onload = initDateRange();
